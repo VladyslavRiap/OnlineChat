928993922be1278d3572663794216c49184bb3b2
unknown <uchuha1997@gmail.com>
2025-06-30 17:41:15 +0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(5001, () => {
+  console.log("server running on port:" + PORT);
+});
